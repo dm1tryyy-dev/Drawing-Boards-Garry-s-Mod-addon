d9947febd9d493ec44eabc28dcc1c5680e5fce1d
dm1tryyy-dev <ostanind947@gmail.com>
2026-05-31 23:47:18 +0500

    Небольшие изменения

--- lua/autorun/server/boards_autorun.lua
+++ lua/autorun/server/boards_autorun.lua
@@ -1,10 +1,28 @@
 util.AddNetworkString("DrawingBoardsWelcome")
 
+-- ==== ADDON SETTINGS ====
+workshop_ids = {"3595295430"} --write IDs here
+
+if SERVER and not game.SinglePlayer() then
+    for _, id in pairs(workshop_ids) do
+        if id and id ~= "" then resource.AddWorkshop(id) end
+    end
+end
+
+
 if SERVER then
+    print("######################################################################")
+    print("    ***    Welcome to the Drawing Boards | Addon is active!    ***    ")
+    print("######################################################################")
+    
     hook.Add("PlayerInitialSpawn", "WelcomeDrawingBoards", function(ply)
         if IsValid(ply) and ply:IsPlayer() then
-            net.Start("DrawingBoardsWelcome")
-            net.Send(ply)
+            timer.Simple(2, function()
+                if IsValid(ply) then
+                    net.Start("DrawingBoardsWelcome")
+                    net.Send(ply)
+                end
+            end)
         end
     end)
 end
